d6d1e57197f5ee21c05640f73b563012b40a7b6b
Mayaka Donnicias <mayakadonnicias@gmail.com>
2019-09-25 09:06:19 +0300

    Add project files.

---
+++ EmployeesHierarchy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesHierarchy
+{
+    public class Class1
+    {
+    }
+}
